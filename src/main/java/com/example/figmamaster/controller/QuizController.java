1601b6a0db5622a76ea900418399439f4c6f4d96
darkopopovski <darkopopovski39@yahoo.com>
2022-08-28 22:29:20 +0200

    adding quiz

---
+++ src/main/java/com/example/figmamaster/controller/QuizController.java
@@ -0,0 +1,23 @@
+package com.example.figmamaster.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/quiz")
+@Controller
+public class QuizController {
+
+    @GetMapping
+    public String getRegisterPage(@RequestParam(required = false) String error, Model model) {
+        if(error != null && !error.isEmpty()) {
+            model.addAttribute("hasError", true);
+            model.addAttribute("error", error);
+        }
+        model.addAttribute("bodyContent", "quiz");
+        return "master-template";
+    }
+}
